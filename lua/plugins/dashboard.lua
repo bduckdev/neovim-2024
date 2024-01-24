4a5762bbb59958cfb51e43fd51282b79d4519fc2
Brennan Duck <brennantduck@gmail.com>
2024-01-24 09:54:13 -0500

    updated dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,30 +1,42 @@
 return {
-    "goolord/alpha-nvim",
-    dependencies = {
-        "nvim-tree/nvim-web-devicons",
-    },
-    config = function()
-        local alpha = require("alpha")
-        local dashboard = require("alpha.themes.startify")
+	"goolord/alpha-nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		local alpha = require("alpha")
+		local dashboard = require("alpha.themes.dashboard")
 
-        dashboard.section.header.val = {
-            [[                                                                       ]],
-            [[                                                                       ]],
-            [[                                                                       ]],
-            [[                                                                       ]],
-            [[                                                                     ]],
-            [[       ████ ██████           █████      ██                     ]],
-            [[      ███████████             █████                             ]],
-            [[      █████████ ███████████████████ ███   ███████████   ]],
-            [[     █████████  ███    █████████████ █████ ██████████████   ]],
-            [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
-            [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
-            [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
-            [[                                                                       ]],
-            [[                                                                       ]],
-            [[                                                                       ]],
-        }
-
-        alpha.setup(dashboard.opts)
-    end,
+		dashboard.section.header.val = {
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                     ]],
+			[[       ████ ██████           █████      ██                     ]],
+			[[      ███████████             █████                             ]],
+			[[      █████████ ███████████████████ ███   ███████████   ]],
+			[[     █████████  ███    █████████████ █████ ██████████████   ]],
+			[[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+			[[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+			[[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+		}
+		dashboard.section.buttons.val = {
+			dashboard.button("SPC p v", " Current Directory"),
+			dashboard.button("SPC p f", "󰱼 Find file"),
+			dashboard.button("q", ") Quit", ":q<CR>"),
+		}
+		alpha.setup(dashboard.opts)
+	end,
 }
